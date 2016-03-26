9261e072ddeb7a5328a22abb8a89d3de932ea386
Jan Pevzner <jan.pevzner@gothamblock.com>
2016-03-26 12:57:55 -0400

    minor changes

--- plot1.R
+++ plot1.R
@@ -1,7 +1,7 @@
 
 rm(list=ls())
 
-D = read.csv("subset.txt", sep=";")
+D = read.csv("subset.txt", sep=";", na.strings = "?", stringsAsFactor = FALSE)
 
 t = strptime(paste(D[,1],D[,2]), "%m/%d/%Y %H:%M:%S", tz = "PDT")
 
